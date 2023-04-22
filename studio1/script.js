b894e62c0c3445627004f57b0097b69d668fb593
kesttrel <122503015+kesttrel@users.noreply.github.com>
2023-04-21 22:10:16 -0700

    Added Frog Loading Screen

--- studio1/script.js
+++ studio1/script.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict'
     console.log('reading js');
 
@@ -8,17 +8,23 @@
     const line2 = document.querySelector('#line2');
     const line3 = document.querySelector('#line3');
     const line4 = document.querySelector('#line4');
-    
+
     const poem = {
         start: [0, 5, 10, 15],
         stop: [4, 9, 13, 17],
         line: [line1, line2, line3, line4]
     }
 
+    const loading = document.querySelector('.fa-frog');
+
+    myVideo.addEventListener('playing', function () {
+        loading.style.display = 'none';
+    })
+
     const intervalID = setInterval(checkTime, 1000);
 
     function checkTime() {
-        for ( let i = 0; i < poem.start.length; i++ ) {
+        for (let i = 0; i < poem.start.length; i++) {
             if (poem.start[i] < myVideo.currentTime && myVideo.currentTime < poem.stop[i]) {
                 poem.line[i].className = 'showing'
             } else {
@@ -29,7 +35,7 @@
 
     const myInput = document.querySelector('#myInput');
 
-    myInput.addEventListener('change', function(){
+    myInput.addEventListener('change', function () {
         if (myInput.checked) {
             overlay.className = 'showing';
             overlay.className.remove('hidden');
