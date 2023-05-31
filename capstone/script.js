50a6a3474a1b520760c4001cc12276832c7454cb
kesttrel <122503015+kesttrel@users.noreply.github.com>
2023-05-30 20:15:38 -0700

    ViewPost initial build and beginning of nav for mobile

--- capstone/script.js
+++ capstone/script.js
@@ -3,16 +3,17 @@ Parse.initialize('jZbgGvTqompDKrt8ffrLnnZD7yY4DIhkcJEiAw5W', 'LINDPSjtUUuLwQits4
 Parse.serverURL = 'https://parseapi.back4app.com/';
 AOS.init();
 
-(function(){
+(function () {
     'use strict';
 
     const newBtn = document.getElementById('newbtn');
     const postList = document.querySelector('main ol');
     const createPostForm = document.getElementById('create-post');
     const inputs = document.querySelectorAll('#create-post input:not([type=submit])');
+    const viewPostOverlay = document.getElementById('view-post');
+    const navMenu = document.getElementsByTagName('nav');
 
-
-    async function displayPosts(){
+    async function displayPosts() {
         const posts = Parse.Object.extend('Posts');
         const query = new Parse.Query(posts);
         try {
@@ -24,53 +25,77 @@ AOS.init();
                 const response = eachPost.get('response');
                 const signature = eachPost.get('signature');
 
+                //Creates the card in the post grid
                 const theListItem = document.createElement('li');
+                theListItem.className = 'post-list'
                 theListItem.setAttribute('id', `r-${id}`);
                 theListItem.setAttribute('data-aos', 'fade-up');
                 theListItem.setAttribute('data-aos-duration', '1500');
                 theListItem.innerHTML =
-                `<div class="li-wrapper">
-                    <div></div>
-                    <h3 class="title">${title}</h2>
-                    <p class="signature">-${signature}</p>
-                    <div class="viewBtn">view post</div>
+                    `<div class="li-wrapper">
+                    <div class="post-img"></div>
+                    <h3>${title}</h2>
+                    <p>-${signature}</p>
+                    <div class="viewbtn">view post</div>
                 </div>`;
                 // `<h2 class="title">${title}</h2>
                 // <p class="response">${response}</p>
                 // <p class="signature">-${signature}</p>`;
                 postList.append(theListItem);
+
+                const postContent = document.createElement('div');
+                postContent.setAttribute('id', `r-${id}`);
+                postContent.innerHTML =
+                    `<i class="fas fa-times-circle fa-2x"></i>
+                    <div id="view-post-content">
+                        <div class="post-img"></div>
+                        <h3 >${title}</h2>
+                        <p>${response}</p>
+                        <p>-${signature}</p>
+                    </div>
+                    <div id="view-post-nav">
+                        <div>
+                            <i class="fa fa-caret-left fa-2x"></i>
+                            previous post
+                        </div>
+                        <div>
+                            next post
+                            <i class="fa fa-caret-right fa-2x"></i>
+                        </div>
+                    </div>`;
+                viewPostOverlay.append(postContent);
             });
-        } catch(error) {
+        } catch (error) {
             console.error('error while fetching posts', error);
         }
     }
     displayPosts();
-    
-    newBtn.addEventListener('click', function(event){
+
+    newBtn.addEventListener('click', function (event) {
         event.preventDefault();
         createPostForm.className = 'create-post-onscreen';
     });
 
-    createPostForm.addEventListener('submit', function(event){
+    createPostForm.addEventListener('submit', function (event) {
         event.preventDefault();
         createPost();
     });
 
-    async function createPost(){
+    async function createPost() {
         const newPost = {};
 
-        for(let i=0; i<inputs.length; i++){
+        for (let i = 0; i < inputs.length; i++) {
             let key = inputs[i].getAttribute('name');
             let value = inputs[i].value;
             newPost[key] = value;
         }
-        if(newPost.title !='' && newPost.response !='' && newPost.signature !=''){
+        if (newPost.title != '' && newPost.response != '' && newPost.signature != '') {
             // alert('added');
             const newPostData = new Parse.Object('Posts');
             newPostData.set('title', newPost.title);
             newPostData.set('response', newPost.response);
             newPostData.set('signature', newPost.signature);
-            try{
+            try {
                 // add to B4A
                 const result = await newPostData.save();
                 console.log('post created', result);
@@ -80,9 +105,9 @@ AOS.init();
                 displayPosts();
                 // Close the form 
                 createPostForm.className = 'create-post-offscreen';
-            } catch(error) {
+            } catch (error) {
                 console.error('Error while creating friend:', error);
-            } 
+            }
         } else {
             // createPostForm.className = 'create-post-offscreen';
             alert('one or more fields left empty');
@@ -90,14 +115,19 @@ AOS.init();
     }
 
     // Event Delegation 
-    document.addEventListener('click', function(event) {
-        if(event.target.matches('.fa-times-circle')) {
+    document.addEventListener('click', function (event) {
+        if (event.target.matches('.fa-times-circle')) {
             createPostForm.className = 'create-post-offscreen';
+            viewPostOverlay.className = "hidden";
+        }
+        if (event.target.matches('.viewbtn')) {
+            console.log('view button clicked');
+            viewPostOverlay.className = "showing";
         }
     });
 
     // Helper functions
-    function resetFormFields(){
+    function resetFormFields() {
         document.getElementById('title').value = '';
         document.getElementById('response').value = '';
         document.getElementById('signature').value = '';
