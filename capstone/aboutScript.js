7e7c53914ba1da4aec128c5633e89fd6c952e79b
kesttrel <122503015+kesttrel@users.noreply.github.com>
2023-06-15 01:40:44 -0700

    map added but not displaying

--- capstone/aboutScript.js
+++ capstone/aboutScript.js
@@ -1,1 +1,13 @@
 AOS.init();
+(function() {
+    'use strict'
+    // Map 
+    const map = L.map('map').setView([40.863428, -124], 10);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    
+    const marker1 = L.marker([40.857345, -124.053789]).addTo(map);
+    marker1.bindPopup('<b>Woozy in the forest</b><br><p>12/26/22</p><br><img src="images/woozy-forest.jpg">');
+});
